34e6abc10922272c200db131a44a5df38e45e2e2
Zakariae EL YOUSFI <76444538+zakriae@users.noreply.github.com>
2024-09-29 23:54:14 +0200

    Adding Clerk Auth

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUnPage = () => {
+  return <SignUp />;
+};
+
+export default SignUnPage;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <UserButton />
+    </div>
+  );
+};
+
+export default Home;
