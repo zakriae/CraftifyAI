8650ce3d46ac43516b378d26aaf30bb1ca8e6a5e
Zakariae EL YOUSFI <76444538+zakriae@users.noreply.github.com>
2024-10-05 13:23:58 +0100

    Adding the dynamique header for the tronsfo types

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/shared/header";
 import React from "react";
+import { transformationTypes } from "@/components/constants";
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+const AddTransformationType = ({params: {type}}: SearchParamProps) => {
+
+  const transformation: any = transformationTypes[type];
+  return <div>
+      <Header title={transformation.title} subtitle={transformation.subTitle}/>
+  </div>;
 };
 
 export default AddTransformationType;
